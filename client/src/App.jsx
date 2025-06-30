e5215723a3551a89ad5589349ef759347fec091c
s3lftitled <jjaviendemonteverde@gmail.com>
2025-06-30 16:08:23 +0800

    App Level: Added new routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,14 +3,21 @@ import { Suspense, lazy } from 'react'
 import './App.css'
 
 const HalimuyakLanding = lazy(() => import('./pages/Landing/LandingPage'))
+const AuthContainer = lazy(() => import('./pages/Authentication/AuthContainer'))
+const BrandPage = lazy(() => import('./pages/Collaborators/BrandPage'))
+const GoogleSuccess = lazy(() => import('./components/GoogleSuccess/GoogleSuccess'))
 
 function App() {
-
   return (
     <Router>
-      <Routes>
-        <Route path='/' element={<HalimuyakLanding />} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path='/' element={<HalimuyakLanding />} />
+          <Route path='/authentication' element={<AuthContainer />} />
+          <Route path='/brands' element={ <BrandPage />} />
+          <Route path="/google-success" element={<GoogleSuccess />} />
+        </Routes>
+      </Suspense>
     </Router>
   )
 }
