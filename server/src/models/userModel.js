d26e1b6e02905532158155fb16552263c4732ccf
s3lftitled <jjaviendemonteverde@gmail.com>
2025-06-30 16:11:41 +0800

    User Model: Added new attributes

--- server/src/models/userModel.js
+++ server/src/models/userModel.js
@@ -3,6 +3,10 @@ const mongoose = require('mongoose')
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, minlegnth: 3, maxlength: 40},
   email: { type: String, require: true, unique: true },
+  password: { type: String },
+  verificationCode: { type: String },
+  isVerified: { type: Boolean, default: false },
+  verificationCodeExpiresAt: { type: Date, default: null }
 })
 
 module.exports = mongoose.model('UserCollection', userSchema)
